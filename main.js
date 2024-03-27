2a143ab0d33313c881e50e0e33ebf44ccaf79e6f
Tim Laurie <165136490+dartsterner@users.noreply.github.com>
2024-03-27 09:30:35 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+  return function (arr, k) {
+      res = [];
+      combinations(arr, k, 0, 0, []);
+      var temp = res;
+      // Free the extra memory
+      res = null;
+      return temp;
+    };
+  }());
+
+  exports.combinations = combinations;
+
+}((typeof window === 'undefined') ? module.exports : window));
+
+
+ function permutations(arr, current) {
+      if (current >= arr.length) {
+        return res.push(arr.slice());
+      }
+      for (var i = current; i < arr.length; i += 1) {
+        swap(arr, i, current);
+        permutations(arr, current + 1);
+        swap(arr, i, current);
+      }
+    }
